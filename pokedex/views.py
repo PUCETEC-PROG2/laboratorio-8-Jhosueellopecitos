7681cb1f836070fd4887bebbaa0b067e389273a3
Jhosueellopecitos <152736799+Jhosueellopecitos@users.noreply.github.com>
2024-08-02 00:11:13 -0500

    Update views.py

--- pokedex/views.py
+++ pokedex/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.views import LoginView
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 from pokedex.forms import PokemonForm
-from .models import Pokemon
+from .models import Pokemon, Trainer
 
 def index(request):
     pokemons = Pokemon.objects.order_by('type')
@@ -16,6 +16,14 @@ def pokemon(request, pokemon_id):
     template = loader.get_template('display_pokemon.html')
     context = {
         'pokemon': pokemon
+    }
+    return HttpResponse(template.render(context, request))
+
+def trainer(request, trainer_id):
+    trainer = get_object_or_404(Trainer, pk = trainer_id)
+    template = loader.get_template('display_trainer.html')
+    context = {
+        'trainer': trainer
     }
     return HttpResponse(template.render(context, request))
 
@@ -34,6 +42,21 @@ def add_pokemon(request):
 class CustomLoginView(LoginView):
     template_name= 'login.html'
 
+
+@login_required
+def add_trainer(request):
+    if request.method =='POST':
+        form = TrainerForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('pokedex:index')
+    else:
+        form = TrainerForm()
+    return render(request, 'trainer_form.html', {'form':form})
+
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+
 @login_required
 def edit_pokemon(request, id):
     pokemom = get_object_or_404(Pokemon, pk = id)
@@ -48,8 +71,27 @@ def edit_pokemon(request, id):
     return render(request, 'pokemon_form.html', {'form': form})
 
 @login_required
+def edit_trainer(request, trainer_id):
+    trainer = get_object_or_404(Trainer, pk = trainer_id) 
+    if request.method =='POST':
+        form = TrainerForm(request.POST, request.FILES, instance=trainer)
+        if form.is_valid():
+            form.save()
+            return redirect('pokedex:list_index')
+    else:
+        form = TrainerForm(instance=trainer)
+    return render(request, 'trainer_form.html', {'form':form})
+    
+
+@login_required
 def delete_pokemon(request, id):
     pokemom = get_object_or_404(Pokemon, pk = id)
     pokemom.delete()
     return redirect("pokedex:index")
     
+    
+@login_required
+def delete_trainer(request, trainer_id):
+    trainer = get_object_or_404(Trainer, pk = trainer_id)
+    trainer.delete()
+    return redirect("pokedex:index")
