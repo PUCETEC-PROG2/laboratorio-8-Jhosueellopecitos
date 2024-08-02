6abcd739211cd073369b4f67bb974e718e99af59
Jhosueellopecitos <152736799+Jhosueellopecitos@users.noreply.github.com>
2024-08-02 00:02:24 -0500

    Update urls.py

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     path("add_pokemon/", views.add_pokemon, name="add_pokemon"),
     path("edit_pokemon/<int:id>/", views.edit_pokemon, name="edit_pokemon"),
     path("delete_pokemon/<int:id>/", views.delete_pokemon, name="delete_pokemon"),
+    path("trainer/<int:trainer_id>/", views.trainer, name="trainer"),
+    path("trainer/add_trainer/", views.add_trainer, name="add_trainer"),
+    path("trainer/edit/<int:trainer_id>/", views.edit_trainer, name="edit_trainer"),
+    path("trainer/delete/<int:trainer_id>/", views.delete_trainer, name="delete_trainer"),
     path('login/', views.CustomLoginView.as_view(), name='login')
 ]
